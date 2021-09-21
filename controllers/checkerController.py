563ff2d5b5f51a147456e7cbfd85a77c2c9c9e62
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-09-21 14:36:27 +0800

    6

--- controllers/checkerController.py
+++ controllers/checkerController.py
@@ -39,7 +39,7 @@ class Checker:
         project_status = self.db_controller.get_project_status(project_id, locale)
         # check if there is project
         if not project_status:
-            print("No member update this project yet")
+            print("No graders update this project yet")
             return False
         # get the init container and assign the value into the container
         project_status_container = self.init_project_status_container()
@@ -48,12 +48,16 @@ class Checker:
         # sorted the dictionary
         sorted_project_status_container = {k: v for k, v in sorted(project_status_container.items(), key=lambda item: item[1])}
         # print status
-        print("\u001b[31;1m{}\u001b[0m".format(project_id))
+        txt = ''
+        txt += "\u001b[34m{}\u001b[0m\n".format(project_id)
+        txt += "{:<20}{:<10}{:<10}{:<10}\n".format("Grader", "mins", "prj id", "locale")
         for name, mins in sorted_project_status_container.items():
             mins = round(mins, 2)
             if mins == self._empty:
-                mins = '--'
-            print("{:<20}{:<7}mins".format(name, mins))
+                txt += "{:<20}{:<10}\n".format(name, '--')
+            else:
+                txt += "{:<20}{:<10}\u001b[32;1m{:<10}{:<10}\u001b[0m\n".format(name, mins, 'matched', 'matched')
+        print(txt)
 
     def update_project_from_txt(self):
         pass
