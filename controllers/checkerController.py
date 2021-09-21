9e56cd4c8b112f0add8590ecd26c7c7b5edfab88
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-09-21 14:40:48 +0800

    6

--- controllers/checkerController.py
+++ controllers/checkerController.py
@@ -49,8 +49,8 @@ class Checker:
         sorted_project_status_container = {k: v for k, v in sorted(project_status_container.items(), key=lambda item: item[1])}
         # print status
         txt = ''
-        txt += "\u001b[34m{}\u001b[0m\n".format(project_id)
-        txt += "{:<20}{:<10}{:<10}{:<10}\n".format("Grader", "mins", "prj id", "locale")
+        txt += "\u001b[1m\n\u001b[34m{}\u001b[0m\u001b[0m\n".format(project_id)
+        txt += "\u001b[1m{:<20}{:<10}{:<10}{:<10}\u001b[0m\n".format("Grader", "mins", "prj id", "locale")
         for name, mins in sorted_project_status_container.items():
             mins = round(mins, 2)
             if mins == self._empty:
