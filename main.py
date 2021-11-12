b97f700778080108eccce9c3b0674b5dacc6aa6a
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-11-12 10:50:40 +0800

    76

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from controllers import gradingController, dbController, webController
 
 command = False
-VERSION = "0.0.20"
+VERSION = "0.0.21"
 
 default_url = "https://crowdcollect2.siri.apple.com/main/project/"
 db_controller = dbController.Database()
